9eb737bc0ecfc9be0e19c17ac06951a54a5f5d9f
lucarin91 <lucarin@protonmail.com>
2026-03-18 21:21:44 +0100

    fix assets mounting

--- src/arduino/app_bricks/web_ui/web_ui.py
+++ src/arduino/app_bricks/web_ui/web_ui.py
@@ -253,8 +253,8 @@ class WebUI:
             methods=["GET"],
             name="index",
         )
+        self.app.mount("/webui/libs", NonCachedStaticFiles(directory=os.path.join(os.path.dirname(__file__), "jslibs")), name="static-jslibs")
         self.app.mount(url_path, NonCachedStaticFiles(directory=self._assets_dir_path, html=True), name="static")
-        self.app.mount("/libs/", NonCachedStaticFiles(directory=os.path.join(os.path.dirname(__file__), "libs")), name="libs-static")
 
     def _init_socketio(self):
         @self.sio.on("connect")
